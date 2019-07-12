d5f40f75eb3fd7b621546c2b07687ed888e50cb4
Young-Sook <kys91240@gmail.com>
2019-07-11 20:05:31 -0400

    added matplotlib.use('Agg')

--- CRADLE/CorrectBias/calculateOnebp.pyx
+++ CRADLE/CorrectBias/calculateOnebp.pyx
@@ -10,6 +10,8 @@ import py2bit
 import pyBigWig
 import warnings
 import statsmodels.api as sm
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import random
 import warnings
