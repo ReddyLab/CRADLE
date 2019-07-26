dedc08c2f657344781fa6e13813b57219a0bd0a5
Young-Sook <kys91240@gmail.com>
2019-07-26 10:23:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def main():
 	      classifiers = [
 			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3.6",
-			"Programming Language :: Python :: 3.7"
+			"Programming Language :: Python :: 3.7",
 			"License :: OSI Approved :: MIT License"
 	      ]
 
