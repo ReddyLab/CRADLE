08d5c8d59d68572aeec3f49b0a5a877b612d366f
Thomas Cowart <thomas.cowart@duke.edu>
2021-10-26 14:13:54 -0400

    Makes ctrlbw and expbw optional args across all commands
    The correctBias, correctBias_stored, normalize, and callPeak commands
    all use the ctrlbw and expbw argument, even if the arguments are optional.
    
    the new covariates command doesn't use those arguments at all, which
    kind of broke correctbiasutils/vari.py. This change makes those arguments
    completely optional, only using them and creating the associated global
    variables if the are declared arguments.

--- CRADLE/correctbiasutils/vari.py
+++ CRADLE/correctbiasutils/vari.py
@@ -4,30 +4,44 @@ import pyBigWig # type: ignore
 
 from CRADLE.correctbiasutils import ChromoRegionSet
 
+
+
 def setGlobalVariables(args):
-	global CTRLBW_NAMES
-	global EXPBW_NAMES
-
-	global CTRLBW_NUM
-	global EXPBW_NUM
-	global SAMPLE_NUM
-
 	global NUMPROCESS
 	global OUTPUT_DIR
 	global REGIONS
 
 	### input bigwig files
-	CTRLBW_NAMES = args.ctrlbw
-	EXPBW_NAMES = args.expbw
-	CTRLBW_NUM = len(CTRLBW_NAMES)
-	EXPBW_NUM = len(EXPBW_NAMES)
-	SAMPLE_NUM = CTRLBW_NUM + EXPBW_NUM
-	OUTPUT_DIR = setOutputDirectory(args.o)
-	with pyBigWig.open(CTRLBW_NAMES[0]) as ctrlBW:
-		regionSet = ChromoRegionSet.loadBed(args.r)
-		blacklistRegionSet = ChromoRegionSet.loadBed(args.bl) if "bl" in dir(args) and args.bl else None
-		REGIONS = setAnlaysisRegion(regionSet, blacklistRegionSet, ctrlBW)
+
+	regionSet = ChromoRegionSet.loadBed(args.r)
+	blacklistRegionSet = ChromoRegionSet.loadBed(args.bl) if "bl" in dir(args) and args.bl else None
+
+	if hasattr(args, "ctrlbw"):
+		global CTRLBW_NAMES
+		global CTRLBW_NUM
+
+		CTRLBW_NAMES = args.ctrlbw
+		CTRLBW_NUM = len(CTRLBW_NAMES)
+
+		with pyBigWig.open(CTRLBW_NAMES[0]) as ctrlBW:
+			REGIONS = setAnlaysisRegion(regionSet, blacklistRegionSet, ctrlBW)
+	else:
+		REGIONS = setAnlaysisRegion(regionSet, blacklistRegionSet)
+
+	if hasattr(args, "expbw"):
+		global EXPBW_NAMES
+		global EXPBW_NUM
+
+		EXPBW_NAMES = args.expbw
+		EXPBW_NUM = len(EXPBW_NAMES)
+
+	if hasattr(args, "ctrlbw") and hasattr(args, "expbw"):
+		global SAMPLE_NUM
+
+		SAMPLE_NUM = CTRLBW_NUM + EXPBW_NUM
+
 	NUMPROCESS = setNumProcess(args.p)
+	OUTPUT_DIR = setOutputDirectory(args.o)
 
 
 def setOutputDirectory(outputDir):
@@ -36,25 +50,29 @@ def setOutputDirectory(outputDir):
 
 	return outputDir.rstrip("/")
 
-def setAnlaysisRegion(regionSet, blacklistRegionSet, ctrlBW):
+
+def setAnlaysisRegion(regionSet, blacklistRegionSet, ctrlBW=None):
 	regionSet.mergeRegions()
 
 	if blacklistRegionSet is not None:
 		blacklistRegionSet.mergeRegions()
-		regionSetWoBL = regionSet - blacklistRegionSet
+		untrimmedRegionSet = regionSet - blacklistRegionSet
 	else:
-		regionSetWoBL = regionSet
+		untrimmedRegionSet = regionSet
 
-	finalRegionSet = ChromoRegionSet()
-	for region in regionSetWoBL:
-		chromoLen = ctrlBW.chroms(region.chromo)
-		if chromoLen is None or chromoLen <= region.start:
-			continue
+	if ctrlBW is not None:
+		finalRegionSet = ChromoRegionSet()
+		for region in untrimmedRegionSet:
+			chromoLen = ctrlBW.chroms(region.chromo)
+			if chromoLen is None or chromoLen <= region.start:
+				continue
 
-		if region.end > chromoLen:
-			region.end = chromoLen
+			if region.end > chromoLen:
+				region.end = chromoLen
 
-		finalRegionSet.addRegion(region)
+			finalRegionSet.addRegion(region)
+	else:
+		finalRegionSet = untrimmedRegionSet
 
 	return finalRegionSet
 
